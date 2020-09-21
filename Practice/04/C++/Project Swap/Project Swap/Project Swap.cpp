6b7d0481e6cab0377cd6a27040453f5687bf916f
VadimKovalenko <4Krio9Gen3@gmail.com>
2020-09-21 22:59:07 +0300

    Change to Project

---
+++ Practice/04/C++/Project Swap/Project Swap/Project Swap.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float a, b;
+    cout << "Введите два значения: ";
+    if (cin >> a >> b)
+    {
+        cout << "Оригинальные значения: a = " << a << ", b = " << b << std::endl;
+        int tmp = a;
+        a = b;
+        b = tmp;
+        cout << "После смены: a = " << a << ", b = " << b << std::endl;
+    }
+    else
+    {
+        cout << "Ошибка" << std::endl;
+    }
+}
